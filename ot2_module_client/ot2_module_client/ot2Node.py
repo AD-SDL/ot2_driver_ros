fad6717b85c4118b033041aa0823ab59311701e2
Kyle Hippe <kyle.hippe@gmail.com>
2022-12-08 15:34:21 -0600

    trying to turn off lights when disconected

--- ot2_module_client/ot2_module_client/ot2Node.py
+++ ot2_module_client/ot2_module_client/ot2Node.py
@@ -4,16 +4,16 @@
 import yaml
 from typing import List, Tuple
 from pathlib import Path
-from datetime import datetime 
-from copy import deepcopy 
+from datetime import datetime
+from copy import deepcopy
 
 
-import rclpy  
+import rclpy
 from ot2_driver.ot2_driver_http import OT2_Config, OT2_Driver
 import opentrons.simulate
 from opentrons.simulate import format_runlog
 
-from rclpy.node import Node 
+from rclpy.node import Node
 from wei_services.srv import WeiActions, WeiDescription
 from std_msgs.msg import String
 from rclpy.executors import MultiThreadedExecutor
@@ -29,42 +29,43 @@ class ot2Node(Node):
     Inside the function the parameters exist, and calls to other functions and services are made so they can be executed in main.
     """
 
-
     def __init__(self, NODE_NAME, ROBOT_IP):
         """Setup OT2 node"""
 
         super().__init__(NODE_NAME)
 
-        self.ot2 = OT2_Driver(OT2_Config(ip=ROBOT_IP))            
-        self.get_logger().info("OT2 is online") # Wakeup Message
-        
+        self.ot2 = OT2_Driver(OT2_Config(ip=ROBOT_IP))
+        self.get_logger().info("OT2 is online")  # Wakeup Message
+
         self.description = {
-            'name': NODE_NAME,
-            'type':'',
-            'actions':
-            {
-                'execute': "config : %s", ## takes in the yaml content as second string arg
-                'run_protocol': "config_path: %s",  ## Temp inclusion
-            }
+            "name": NODE_NAME,
+            "type": "",
+            "actions": {
+                "execute": "config : %s",  ## takes in the yaml content as second string arg
+                "run_protocol": "config_path: %s",  ## Temp inclusion
+            },
         }
 
-        self.timer_period = 1   # seconds
-        
+        self.timer_period = 1  # seconds
+
         ## Creating state Msg as a instance variable
-        self.stateMsg = String()    
-        self.state = "READY"        ## If we get here without error, the client is initialized
+        self.stateMsg = String()
+        self.state = "READY"  ## If we get here without error, the client is initialized
         self.stateMsg.data = "State %s" % self.state
 
         # Publisher for ot2 state
-        self.statePub = self.create_publisher(String, "ot2_state", 10)   
+        self.statePub = self.create_publisher(String, "ot2_state", 10)
 
         # Timer callback publishes state to namespaced ot2_state
-        self.stateTimer = self.create_timer(self.timer_period, self.stateCallback)   
+        self.stateTimer = self.create_timer(self.timer_period, self.stateCallback)
 
         # Control and discovery services
-        self.actionSrv = self.create_service(WeiActions, NODE_NAME + "/action_handler", self.actionCallback)
-        self.descriptionSrv = self.create_service(WeiDescription, NODE_NAME + "/description_handler", self.descriptionCallback)
-
+        self.actionSrv = self.create_service(
+            WeiActions, NODE_NAME + "/action_handler", self.actionCallback
+        )
+        self.descriptionSrv = self.create_service(
+            WeiDescription, NODE_NAME + "/description_handler", self.descriptionCallback
+        )
 
     def descriptionCallback(self, request, response):
         """The descriptionCallback function is a service that can be called to showcase the available actions a robot
@@ -84,8 +85,6 @@ class ot2Node(Node):
 
         return response
 
-
-
     async def actionCallback(self, request: str, response: str) -> None:
 
         """The actions the robot can perform, also performs them
@@ -99,15 +98,15 @@ class ot2Node(Node):
         -------
         None
         """
-        
-        self.manager_command = request.action_handle        
-        self.manager_vars = eval(request.vars) 
+
+        self.manager_command = request.action_handle
+        self.manager_vars = eval(request.vars)
 
         self.get_logger().info(f"In action callback, command: {self.manager_command}")
 
         # if "execute" == self.manager_command:
-            
-        #     protocol_config = self.manager_vars.get("config", None) 
+
+        #     protocol_config = self.manager_vars.get("config", None)
         #     if protocol_config:
         #         payload = self.manager_vars.get("payload", None)
         #         self.get_logger().info(f"{self.manager_vars=}")
@@ -121,27 +120,27 @@ class ot2Node(Node):
         if "run_protocol" == self.manager_command:
 
             # protocol_config = self.manager_vars.get("config", None)
-            protocol_config = self.manager_vars.get("config_path", None) 
+            protocol_config = self.manager_vars.get("config_path", None)
             if protocol_config:
                 config_file_path = self.download_config(protocol_config)
-                payload = deepcopy(self.manager_vars) 
+                payload = deepcopy(self.manager_vars)
                 payload.pop("config_path")
                 self.get_logger().info(f"{self.manager_vars=}")
                 self.get_logger().info(f"ot2 {payload=}")
                 self.get_logger().info(f"config_file_path: {config_file_path}")
                 res = self.execute(config_file_path, payload)
                 response.action_response = 1
-                response.action_msg= "all good ot2"
-                self.get_logger().info('Finished Action: ' + request.action_handle)
+                response.action_msg = "all good ot2"
+                self.get_logger().info("Finished Action: " + request.action_handle)
                 return response
 
             else:
-                response.action_msg = "Required 'config' was not specified in request.vars"
+                response.action_msg = (
+                    "Required 'config' was not specified in request.vars"
+                )
                 response.action_response = 0
                 self.get_logger().error(response.action_msg)
                 return response
-            
-
 
     def stateCallback(self):
         """The state of the robot, can be ready, completed, busy, error"""
@@ -150,11 +149,10 @@ class ot2Node(Node):
         self.statePub.publish(self.stateMsg)
         self.get_logger().info(self.stateMsg.data)
 
-
     def download_config(self, protocol_config: str):
-        """ 
+        """
         Saves protocol_config string to a local yaml file locaton
-        
+
         Parameters:
         -----------
         protocol_config: str
@@ -168,21 +166,26 @@ class ot2Node(Node):
 
         # config_dir_path = '/root/config/temp'
         # config_file_path = config_dir_path + "/pc_document.yaml"
-        
+
         config_dir_path = Path.home().resolve() / ".ot2_temp"
         config_dir_path.mkdir(exist_ok=True, parents=True)
-        config_file_path = config_dir_path / f"protocol-{datetime.now().strftime('%Y%m%d-%H%m%s')}.yaml"
-        
-        self.get_logger().info("Writing protocol config to {} ...".format(str(config_file_path)))
-        
-        with open(config_file_path,'w',encoding = "utf-8") as pc_file:
+        config_file_path = (
+            config_dir_path
+            / f"protocol-{datetime.now().strftime('%Y%m%d-%H%m%s')}.yaml"
+        )
+
+        self.get_logger().info(
+            "Writing protocol config to {} ...".format(str(config_file_path))
+        )
+
+        with open(config_file_path, "w", encoding="utf-8") as pc_file:
             yaml.dump(protocol_config, pc_file, indent=4, sort_keys=False)
 
         return config_file_path
 
     def execute(self, protocol_path, payload=None):
-        """ 
-        Compiles the yaml at protocol_path into .py file; 
+        """
+        Compiles the yaml at protocol_path into .py file;
         Transfers and Exececutes the .py file
 
         Parameters:
@@ -195,7 +198,7 @@ class ot2Node(Node):
         response: bool
             If the ot2 execution was successful
         """
-        
+
         ## TODO Should first check that the ot2/node is not in process
         ## TODO must check for /IDLE/AVAILABLE or COMPLETED state
 
@@ -203,24 +206,27 @@ class ot2Node(Node):
         self.stateCallback()
 
         try:
-            self.protocol_file_path, self.resource_file_path = self.ot2.compile_protocol(protocol_path, payload=payload) 
+            (
+                self.protocol_file_path,
+                self.resource_file_path,
+            ) = self.ot2.compile_protocol(protocol_path, payload=payload)
             protocol_file_path = Path(self.protocol_file_path)
-            self.get_logger().info(f"{protocol_file_path.resolve()=}")    
+            self.get_logger().info(f"{protocol_file_path.resolve()=}")
             self.protocol_id, self.run_id = self.ot2.transfer(self.protocol_file_path)
             resp = self.ot2.execute(self.run_id)
-    
+
             if resp["data"]["status"] == "succeeded":
                 self.stateCallback()
                 # self.poll_OT2_until_run_completion()
                 return True
 
         # except FileNotFoundError:
-        #     from pathlib import Path 
-            
+        #     from pathlib import Path
+
         #     response_msg = "Could not find protocol config file at {}, {}".format(protocol_path, Path(protocol_path).exists())
         #     self.get_logger().error(response_msg)
         #     self.stateCallback()
-            
+
         except Exception as e:
             self.state = "ERROR"
             self.stateCallback()
@@ -232,32 +238,38 @@ class ot2Node(Node):
                 on the shared LAN."
                 self.get_logger().error(response_msg)
 
-            response_msg = f'Error: {e}'
+            response_msg = f"Error: {e}"
             self.get_logger().error(response_msg)
-            
-            rclpy.shutdown()  ## TODO: Could alternatively indent into the if block. 
-                              ## TODO: Changed to as is to forestall any unexpected exceptions
+
+            rclpy.shutdown()  ## TODO: Could alternatively indent into the if block.
+            ## TODO: Changed to as is to forestall any unexpected exceptions
 
     def poll_OT2_until_run_completion(self):
-        """ Queries the OT2 run state until reported as 'succeeded' """
+        """Queries the OT2 run state until reported as 'succeeded'"""
 
         self.get_logger().info("Polling OT2 run until completion")
         while self.state != "IDLE":
 
             run_status = self.ot2.get_run(self.run_id)
 
-            if run_status["data"]["status"] and run_status["data"]["status"] == "succeeded":
+            if (
+                run_status["data"]["status"]
+                and run_status["data"]["status"] == "succeeded"
+            ):
                 self.state = "COMPLETED"
                 self.get_logger().info("Stopping Poll")
 
-            elif run_status["data"]["status"] and run_status["data"]["status"] == "running":
+            elif (
+                run_status["data"]["status"]
+                and run_status["data"]["status"] == "running"
+            ):
                 self.state = "BUSY"
-            
+
             self.stateCallback()
             time.sleep(self.timer_period)
 
 
-def main(args=None): 
+def main(args=None):
 
     ip = os.getenv("robot_ip", None)
     node_name = os.getenv("robot_name", None)
@@ -266,10 +278,12 @@ def main(args=None):
         rclpy.init(args=args)  # initialize Ros2 communication
         node = ot2Node(ROBOT_IP=ip, NODE_NAME=node_name)
         try:
-            rclpy.spin(node)    # keep Ros2 communication open for action node
+            rclpy.spin(node)  # keep Ros2 communication open for action node
         except KeyboardInterrupt:
+            node.ot2.change_lights_status(False)
+        except:
             pass
-    
+
     else:
         print("The robot_ip environment variable has not been specified.")
         ## NOTE: TODO TO Verify that the IP Address is correct?
