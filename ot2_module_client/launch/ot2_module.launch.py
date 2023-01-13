8b6dba134e25744ca101aee7abd2244852e1f7ed
Dozgulbas <dozgulbas@anl.gov>
2023-01-13 17:22:28 -0600

    Adding launch parameters (IP, robot name)

--- ot2_module_client/launch/ot2_module.launch.py
+++ ot2_module_client/launch/ot2_module.launch.py
@@ -12,14 +12,32 @@ from os import getenv
 def generate_launch_description():
     ld = LaunchDescription()
 
+    ip = LaunchConfiguration('ip')
+    robot_name = LaunchConfiguration('robot_name')
+
+    declare_use_ip_cmd = DeclareLaunchArgument(
+        name='ip',
+        default_value="146.137.240.35",
+        description='Flag to accept ip address')
+
+    declare_use_robot_name_cmd = DeclareLaunchArgument(
+        name='robot_name',
+        default_value="OT2_alpha",
+        description='Flag to accept robot_name')
+
     ot2 = Node(
             package='ot2_module_client',
             executable='ot2Node',
             namespace = 'std_ns',
-            name=getenv('robot_name'),
+            name=robot_name,
             output='screen',
+            prameters = [
+                {"ip":ip}
+                ],
             emulate_tty = True,
         )
 
+    ld.add_action(declare_use_ip_cmd)
+    ld.add_action(declare_use_robot_name_cmd)
     ld.add_action(ot2)
     return ld
